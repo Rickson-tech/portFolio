a13f6f9399c5bc5cf0b4b6bd0daceb1e5145b4cf
Rickson-tech <59772476+Rickson-tech@users.noreply.github.com>
2023-08-17 17:29:09 +0530

    added about me section to home page

---
+++ force-app/main/default/lwc/skillCards/skillCards.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+export default class SkillCards extends LightningElement {
+    adminImageURL=IMAGES+'/spider2.jpg';
+    trainImageURL=IMAGES+'/spider2.jpg';
+    serviceImageURL=IMAGES+'/spider2.jpg';
+}
